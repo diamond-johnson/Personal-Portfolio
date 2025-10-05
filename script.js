cbf16b7fea1cc8f31ea3b39b32456f3d17019196
Rose <rose729729@gmail.com>
2025-10-05 06:45:08 +0330

    feat: implement responsive navbar with toggle

--- script.js
+++ script.js
@@ -12,23 +12,28 @@ let sections = documents.queryselectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec =>){
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach.apply(Links =>){
+            navLinks.forEach.apply(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            };
+            });
         };
-    };
-}
+    });
 
-// ========================== sticky navbar =============================
-let header = document.queryselector('header');
-header.classList.toggle('sticky', window.ScrollY > 100);
+    // ========================== sticky navbar =============================
+    let header = document.queryselector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-// ========================== remove toggle icon and navbar =============================
+    
+    // ===================== remove toggle icon and navbar ====================
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+
