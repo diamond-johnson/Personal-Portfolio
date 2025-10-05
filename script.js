5629c5f240d5bbd75838bbe56c770b0925679fd5
Rose <rose729729@gmail.com>
2025-10-05 07:12:49 +0330

    refactor: correct typo

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right'});
 
 // ========================== typed js =============================
-const typed = new Typed('.multiple-text', {
+const typed = new typed('.multiple-text', {
     strings: ['Frontend Developer', 'Web Designer', 'Software Engineer', 'Translator', 'Teacher', 'AI specialist'],
     typeSpeed: 70,
     backSpeed: 70,
