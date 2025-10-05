613c68ae2f9f98328d60e123d9f7ec5ba8b2ba31
Rose <rose729729@gmail.com>
2025-10-05 06:57:46 +0330

    feat: add scroll reveal

--- script.js
+++ script.js
@@ -36,4 +36,16 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 
+// ========================== scroll reveal =============================
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right'});
+
 
