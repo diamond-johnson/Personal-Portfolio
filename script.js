9bb26f5b24ef9b776ad8cce4e60d06ec6eb508b7
Rose <rose729729@gmail.com>
2025-10-05 12:23:14 +0330

    refactor: correct syntax error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ========================== toggle icon navbar =============================
-let menuIcon = document.queryselector('#menu-icon');
-let navbar = document.queryselector('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark');
@@ -8,7 +8,7 @@ menuIcon.onclick = () => {
 }
 
 // ========================== scroll section active link =============================
-let sections = documents.queryselectorAll('section');
+let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
@@ -19,15 +19,15 @@ window.onscroll = () => {
         let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach.apply(links => {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
             });
         };
     });
 
     // ========================== sticky navbar =============================
-    let header = document.queryselector('header');
+    let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
     
@@ -49,7 +49,7 @@ ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right'});
 
 // ========================== typed js =============================
-const typed = new typed('.multiple-text', {
+const typed = new Typed('.multiple-text', {
     strings: ['Frontend Developer', 'Web Designer', 'Software Engineer', 'Translator', 'Teacher', 'AI specialist'],
     typeSpeed: 70,
     backSpeed: 70,
